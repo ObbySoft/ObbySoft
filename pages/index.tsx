40c28194e5c2efdfc6566cf1bc4640d66fd8c31d
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-09-06 15:08:25 -0300

    Changed folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<div>
-				<h1 className='text-lg underline'>ObbySoft!</h1>
+				<h1 className='text-lg'>ObbySoft!</h1>
 			</div>
 		</div>
 	);

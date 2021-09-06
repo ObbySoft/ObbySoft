bae73cc503b8a811f885c7dcac1ab6d1c50ffda1
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-09-06 17:04:28 -0300

    Testing changes after ClodFront setup

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,9 @@
 const About = () => {
-	return <div className='mx-auto mt-10 text-center'>This is the about page</div>;
+	return (
+		<div className='mx-auto mt-10 text-center rounded-md shadow-md bg-gray-100 w-3/5 p-10'>
+			This is the about page
+		</div>
+	);
 };
 
 export default About;

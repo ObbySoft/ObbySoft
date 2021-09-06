199d1fff2f977484ed67cdcb23fd0f3efd91df57
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-09-06 14:20:53 -0300

    Initial Setup with tailwindcss

---
+++ obbysoft/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+	return (
+		<div className='py-2 mx-auto w-full bg-gray-300 text-center flex justify-around'>
+			<a href='/'>ObbySoft home page.</a>
+			<a href='/about'>about</a>
+		</div>
+	);
+};
+
+export default Navbar;
---
+++ obbysoft/pages/about.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+	return <div className='mx-auto mt-10 text-center'>This is the about page</div>;
+};
+
+export default About;

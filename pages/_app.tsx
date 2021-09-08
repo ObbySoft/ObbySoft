4de8ea5312e93af595a139638862dfb3f40d0b8d
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-09-08 16:51:08 -0300

    Google Analytics Initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag.js";
 import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<>
 			<Navbar />
@@ -10,4 +25,5 @@ function MyApp({ Component, pageProps }: AppProps) {
 		</>
 	);
 }
+
 export default MyApp;
